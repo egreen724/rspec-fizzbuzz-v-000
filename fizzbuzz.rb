00b6b47a322178860cd372968e2aa765270be886
Erin Green <erin.y.green@gmail.com>
2018-09-05 18:48:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-  
+def fizzbuzz(int)
+  if int % 3 == 0 
+    "Fizz"
+  end
+  elsif int % 5 == 0 
+  "Buzz"
+end
+elsif int % 3 && int % 5 == 0 
+"fizzbuzz"
+end
+else int !% 3 || int !% 5 == 0 
+end
 end 
 
